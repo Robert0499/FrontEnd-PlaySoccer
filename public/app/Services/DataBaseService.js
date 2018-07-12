0f627ead8faf5c987f31645f218ff6022cb87e10
= <=>
2018-07-12 17:51:24 -0500

    Modulos Avanzando

--- public/app/Services/DataBaseService.js
+++ public/app/Services/DataBaseService.js
@@ -1,40 +1,43 @@
-(function() {
-  'use strict';
+angular.module('playsoccer').service('DataBaseService', DataBaseService);
 
-  angular.module('playsoccer').service('DataBaseService', DataBaseService);
+DataBaseService.$inject = ['$http', 'host', '$httpParamSerializerJQLike'];
 
-  DataBaseService.$inject = ['$http', 'host', '$httpParamSerializerJQLike'];
+function DataBaseService($http, host, $httpParamSerializerJQLike) {
+  this.documentos = data => {
+    return $http.get(host + 'tpd');
+  };
+  this.login = data => {
+    return $http.post(host + 'usuarios/auth', $httpParamSerializerJQLike(data));
+  };
+  this.Getequipos = () => {
+    return $http.get(host + 'equipo');
+  };
+  this.getJugadores = id => {
+    return $http({
+      method: 'GET',
+      url: host + 'jugadores/equipo?id=' + id
+    });
+  };
+  this.GetRolPosicion = () => {
+    return $http.get(host + 'rolposicion');
+  };
+  this.AddJugador = data => {
+    return $http.post(host + 'jugador', $httpParamSerializerJQLike(data));
+  };
 
-  function DataBaseService($http, host, $httpParamSerializerJQLike) {
-    this.documentos = data => {
-      return $http.get(host + 'tpd');
-    };
-    this.login = data => {
-      return $http.post(
-        host + 'usuarios/auth',
-        $httpParamSerializerJQLike(data)
-      );
-    };
-    this.Getequipos = () => {
-      return $http.get(host + 'equipo');
-    };
-    this.getJugadores = id => {
-      return $http({
-        method: 'GET',
-        url: host + 'jugadores/equipo?id=' + id
-      });
-    this.GetRolPosicion = () => {
-      return $http.get(host + 'rolposicion');
-    };
-    this.AddJugador = data => {
-      return $http.post(host + 'jugador', $httpParamSerializerJQLike(data));
-    };
-
-    this.RegisterPartido = () => {
-      return $http.get(host + 'partidos/verify');
-    };
-    this.Administrador = data => {
-      return $http.post(host + 'usuario', $httpParamSerializerJQLike(data));
-    };
-  }
-})();
+  this.RegisterPartido = () => {
+    return $http.get(host + 'partidos/verify');
+  };
+  this.Administrador = data => {
+    return $http.post(host + 'usuario', $httpParamSerializerJQLike(data));
+  };
+  this.GetGrupo = () => {
+    return $http.get(host + 'grupo');
+  };
+  this.GetGrupoEquipo = id => {
+    return $http({
+      method: 'GET',
+      url: host + 'equipos/grupo?id=' + id
+    });
+  };
+}
