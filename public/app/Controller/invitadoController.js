824d889be92ad64459f5d89a8842d10ab1a9e8fa
= <=>
2018-07-13 11:42:17 -0500

    mejora visual invitado

--- public/app/Controller/invitadoController.js
+++ public/app/Controller/invitadoController.js
@@ -3,6 +3,9 @@ angular
   .controller('invitadoController', invitadoController);
 invitadoController.$inject = ['$scope', '$state'];
 function invitadoController($scope, $state) {
+  $scope.delegado = function() {
+    $state.go('login');
+  };
   $scope.inicioInvitado = function() {
     $state.go('inicioInvitado');
   };
